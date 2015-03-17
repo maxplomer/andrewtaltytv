547bc608fc5151c5f4ad4c17007be7c02444f566
Max Plomer <maxplomer@gmail.com>
2015-03-17 04:26:16 -0400

    larger social media font

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,6 +3,7 @@ $('document').ready(function() {
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $('body').css('background', 'url("/mobile_bg.jpg")');
     $('#video-controls').css('display', 'none');
+    $('.social-media').css('font-size', '70');
   } else {
     var options = { videoId: 'ab0TSkLe-E0', start: 3 };
     $('#wrapper').tubular(options);
