be7bd7be64d8765c015c9b2adbf9562da834c9ed
Max Plomer <maxplomer@gmail.com>
2015-03-17 04:24:07 -0400

    display none video controls

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,6 +2,7 @@ $('document').ready(function() {
 
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $('body').css('background', 'url("/mobile_bg.jpg")');
+    $('#video-controls').css('display', 'none');
   } else {
     var options = { videoId: 'ab0TSkLe-E0', start: 3 };
     $('#wrapper').tubular(options);
