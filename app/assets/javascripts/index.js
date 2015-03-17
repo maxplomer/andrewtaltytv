dd2920e4de6acac35f9d70401f158d2cb32575a6
Max Plomer <maxplomer@gmail.com>
2015-03-17 04:17:12 -0400

    only set background image if mobile

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,11 @@
 $('document').ready(function() {
-	var options = { videoId: 'ab0TSkLe-E0', start: 3 };
-	$('#wrapper').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
+
+    var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+
+    if (isMobile.matches) {
+    	$('body').css('background', 'url("/mobile_bg.jpg")');
+    } else {
+	  var options = { videoId: 'ab0TSkLe-E0', start: 3 };
+	  $('#wrapper').tubular(options);
+    }
 });
